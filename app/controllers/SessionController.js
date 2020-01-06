c1aa7b33926e6ce8402d4b41aa9aa1adda091a15
danielferreiradf <danielferreiradk@gmail.com>
2020-01-06 08:58:52 -0300

    added verification to routes

--- app/controllers/SessionController.js
+++ app/controllers/SessionController.js
@@ -1,18 +1,30 @@
 const jwt = require('jsonwebtoken');
+const Yup = require('yup');
 const User = require('../models/User');
 
 class SessionController {
   async create(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation failed.' });
+    }
+
     const { email, password } = req.body;
 
     const user = await User.findOne({ where: { email } });
 
     if (!user) {
-      return res.status(401).json({ error: 'User not found' });
+      return res.status(401).json({ error: 'User not found.' });
     }
 
     if (!(await user.checkPassword(password))) {
-      return res.status(401).json({ error: 'Password does not match' });
+      return res.status(401).json({ error: 'Invalid credentials.' });
     }
 
     const { id, name } = user;
--- routes.js
+++ routes.js
@@ -3,9 +3,15 @@ const { Router } = require('express');
 const UserController = require('./app/controllers/UserController');
 const SessionController = require('./app/controllers/SessionController');
 
+const authMiddleware = require('./app/middlewares/auth');
+
 const routes = new Router();
 
+// Users
 routes.post('/users', UserController.create);
+routes.put('/users', authMiddleware, UserController.update);
+
+// Sessions
 routes.post('/sessions', SessionController.create);
 
 module.exports = routes;
