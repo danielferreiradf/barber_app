25271ddda9ab4248a67845dde8c0bf6bf4b65560
danielferreiradf <danielferreiradk@gmail.com>
2020-01-11 11:40:05 -0300

    added pagination to getAll apointments

--- app/controllers/AppointmentController.js
+++ app/controllers/AppointmentController.js
@@ -6,11 +6,14 @@ const File = require('../models/File');
 
 class AppointmentController {
   async getAll(req, res) {
+    const { page = 1 } = req.query;
     try {
       const appointments = await Appointment.findAll({
         where: { user_id: req.userId, canceled_at: null },
         order: ['date'],
         attributes: ['id', 'date'],
+        limit: 20,
+        offset: (page - 1) * 20,
         include: [
           {
             model: User,
